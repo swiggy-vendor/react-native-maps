04b6e934960dd0b8321407ab287000a70ba6c670
harpreetsingh1627484 <harpreet.singh4@swiggy.in>
2025-03-08 12:09:23 +0530

    chore: REmove unwanted null check

--- android/src/main/java/com/rnmaps/maps/MapMarker.java
+++ android/src/main/java/com/rnmaps/maps/MapMarker.java
@@ -296,7 +296,7 @@ public class MapMarker extends MapFeature {
   }
 
   public LatLng interpolate(float fraction, LatLng a, LatLng b) {
-    if (a == null || a.latitude == null || a.longitude == null || b = null || b.latitude == null || b.longitude == null) {
+    if (a == null b = null ) {
       return new LatLng(37.78825, -122.4324);
     }
     double lat = (b.latitude - a.latitude) * fraction + a.latitude;
